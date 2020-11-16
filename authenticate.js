73818fe5265e39d7dd705c1b461e39c6c97c6a60
preethi_hiremath <preethivhiremath.vh@gmail.com>
2020-11-16 12:26:01 +0530

    Oauth

--- authenticate.js
+++ authenticate.js
@@ -4,7 +4,7 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
-
+var FacebookTokenStrategy = require('passport-facebook-token');
 var config = require('./config.js');
 
 passport.use(new LocalStrategy(User.authenticate()));
@@ -36,7 +36,9 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});//verify incommin user
-exports.verifyAdmin = function (req, res, next) {
+
+
+exports.verifyAdmin = function (req, res, next) { //check admin
     if (req.user.admin) {
     next();
     }
@@ -46,3 +48,30 @@ exports.verifyAdmin = function (req, res, next) {
     return next(err);
     }
     };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+        clientID: config.facebook.clientId,
+        clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (!err && user !== null) {
+                return done(null, user);
+            }
+            else {
+                user = new User({ username: profile.displayName });
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err)
+                        return done(err, false);
+                    else
+                        return done(null, user);
+                })
+            }
+        });
+    }
+));
